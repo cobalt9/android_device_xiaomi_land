30ac3e24bb715fc974fa73f8110f8d5dc469bc3c
thp1997 <tirthpatel49@gmail.com>
2017-10-21 00:58:29 +0000

    land: Use only xml audio config

--- product/audio.mk
+++ product/audio.mk
@@ -1,6 +1,5 @@
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(DEVICE_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(DEVICE_PATH)/audio/mixer_paths_qrd_sku1.xml:system/etc/mixer_paths_qrd_sku1.xml \
--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -14,5 +14,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
